3f43ad976bcc40ccfaf76501efa2b5b0f40dcaf3
Chris <jla507@sfu.ca>
2020-09-23 21:23:15 +0800

    regression tree and random forest

--- chris/model_fitting.R
+++ chris/model_fitting.R
@@ -1,15 +1,19 @@
 rm(list = ls())
 setwd("~/Documents/GitHub/stat440-Covid-19/chris")
 library(gdata)
+library(MASS)
+library(rpart)
+library(rpart.plot)
+library(randomForest)
 
 train = read.csv("cleaned_train.csv",sep = ",",header = T)
 test = read.csv("cleaned_test.csv",sep = ",",header = T)
 predict = read.csv("baseline2.txt",sep = ",",header = T)
 
-# Make confirmed a Datetime object
-train$confirmed <- as.Date(train$confirmed, format = "%d.%m.%y")
-test$confirmed <- as.Date(test$confirmed, format = "%d.%m.%y")
 
+# Make confirmed a Datetime object
+train$confirmed <- as.Date(train$confirmed, format = "%Y-%m-%d")
+test$confirmed <- as.Date(test$confirmed, format = "%Y-%m-%d")
 
 # code taken from:https://www.kaggle.com/c/bluebook-for-bulldozers/discussion/4066deal with new levels in test data
 uLevels <- function(names){
@@ -22,13 +26,40 @@ uLevels <- function(names){
     mapLevels(test[,c]) <<- map
   }
 }
-train$country = uLevels('country')
-train$V1 = uLevels('V1')
+uLevels(c('country','V1','sex'))
+
+# Train-valid split
+train_index = sample(1:nrow(train),0.7*nrow(train))
+train_data = train[train_index,]
+valid_data = train[-train_index,]
 
 # Fit model
-fit = lm(duration ~ .,data = train)
-predict$duration = predict(fit,newdata = test)
+model.linear = lm(duration ~ .,data = train_data)
+# MSE of the train data
+mean((predict(model.linear,data = train_data) - train_data$duration)^2)
+# MSE of the valid data
+mean((predict(model.linear,newdata = valid_data) - valid_data$duration)^2)
+predict$duration = predict(model.linear,newdata = test)
+# write.csv(predict,"predicted.csv",row.names=FALSE)
+
+model.tree <- rpart(duration ~ ., data = train_data)
+# MSE of the train data
+mean((predict(model.tree,data = train_data) - train_data$duration)^2)
+# MSE of the valid data
+mean((predict(model.tree,newdata = valid_data) - valid_data$duration)^2)
+
+predict$duration = predict(model.tree,newdata = test)
+# write.csv(predict,"predicted.csv",row.names=FALSE)
 
 
-write.csv(predict,"predicted.csv",row.names=FALSE)
-colnames(predict)
+
+
+# Random Forest Regression
+model.forest = randomForest(duration ~ ., data = train_data)
+# MSE of the train data
+mean((predict(model.forest,data = train_data) - train_data$duration)^2)
+# MSE of the valid data
+mean((predict(model.forest,newdata = valid_data) - valid_data$duration)^2)
+
+predict$duration = predict(model.forest,newdata = test[,])
+# write.csv(predict,"predicted.csv",row.names=FALSE)
